66b3d4a8cd1a357e5654d877f4eae02ffc712cbd
Tapman104 <riddhishnknight@gmail.com>
2026-04-15 11:54:00 +0530

    Update targetSdk to 34 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.openbridge"
         minSdk = 26
-        targetSdk = 29
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
